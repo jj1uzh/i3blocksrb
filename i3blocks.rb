a2e6e2654944f915de8d61e573f69bd167d1d735
Futa Miyachi <miyachf@gmail.com>
2021-10-07 11:49:12 +0900

    click

--- i3blocks.rb
+++ i3blocks.rb
@@ -3,53 +3,65 @@
 require 'json'
 
 $blocks = []
+
 def $blocks.publish
   print '[', self.join(', '), "],\n"
   STDOUT.flush
 end
 
 class Block
-  def initialize(name, conf)
-    @command, @transformer, interval, props = conf.instance_eval {[@command, @transformer, @interval, @props]}
+  def initialize(instance, conf)
+    @command, @transformer, interval, props, onclick = conf.instance_eval {[@command, @transformer, @interval, @props, @onclick]}
     props ||= {}
-    @block_obj = {instance: name, full_text: '', **props}
+    @instance = instance
+    @block_obj = {instance: instance, full_text: '', **props}
     @transformer ||= proc(&:chomp)
     unless @command.nil?
       case interval
-      in 'once' | :once | nil ; exec_once
-      in 'persist' | :persist ; set_persist
-      in Integer => n         ; set_interval_by_secs(n)
-      in Object => other      ; raise "Unknown interval `#{other}` of block `#{name}`"
+      in 'once' | :once | nil then exec_once
+      in 'persist' | :persist then set_persist
+      in Integer => n         then set_interval_by_secs(n)
+      in Object => other      then raise "Unknown interval `#{other}` of block `#{name}`"
       end
     end
+    set_onclick onclick
     regen_json
   end
 
+  attr_reader :instance
   def to_s = @buf
 
   def regen_json
     @buf = JSON.generate @block_obj
   end
 
-  def update_text(command_result)
-    case @transformer[command_result]
-    in String => s ; @block_obj[:full_text] = s
-    in Hash => h   ; @block_obj.merge! h
+  def update(content)
+    case content
+    in String => s then @block_obj[:full_text] = s
+    in Hash => h then @block_obj.merge! h
     end
     regen_json
     $blocks.publish
   end
 
+  def update_by_command
+    update @transformer[`#{@command}`]
+  end
+
   def exec_once
-    update_text `#{@command}`
+    update_by_command
+  rescue
+    update 'ERROR'
   end
 
   def set_interval_by_secs(secs)
     Thread.new do
       loop {
-        update_text `#{@command}`
+        update_by_command
         sleep secs
       }
+    rescue
+      update 'ERROR'
     end
   end
 
@@ -57,10 +69,23 @@ class Block
     Thread.new do
       r, w = IO.pipe
       pid = spawn @command, out:w
-      r.each chomp:true, &method(:update_text)
+      r.each chomp:true do |out|
+        update @transformer[out]
+      end
+    rescue
+      update 'ERROR'
     ensure
       Process.kill :TERM, pid
     end
+  end
+
+  def set_onclick(onclick)
+    fn = case onclick
+         in String => cmd then proc {|obj| spawn cmd}
+         in Proc => p     then proc {|obj| update p[obj]}
+         in nil           then proc {}
+         end
+    define_singleton_method 'on_click', &fn
   end
 end
 
@@ -77,6 +102,10 @@ class BlockConf
 
   def interval(intv)
     @interval = intv
+  end
+
+  def onclick(obj = nil, &block)
+    @onclick = if obj.nil? then block else obj end
   end
 
   valid_props = %w(full_text short_text color background border border_top border_right
@@ -103,6 +132,34 @@ ConfLoader = Class.new do
   end
 end
 
+def click_events
+  Enumerator.new {|y|
+    buf = ""
+    STDIN.each_char do |c|
+      buf << c
+      next if c != '}' # every event is object
+      begin
+        parsed = JSON.parse buf
+        y << parsed
+        STDIN.each_char.take_while {|c| c != ','}
+        buf.clear
+      rescue
+        next
+      end
+    end
+  }
+end
+
+def click_events_loop
+  if (i = STDIN.readchar) != '[' then raise "Invalid input: #{i}" end
+  click_events.each do |obj|
+    next if obj['instance'].nil?
+    $blocks
+      .find {|b| b.instance.to_s == obj['instance']}
+      &.on_click(obj)
+  end
+end
+
 ## main
 begin
   conf_path, = ARGV
@@ -111,7 +168,7 @@ begin
   puts '['
   ConfLoader.new.instance_eval open(File.expand_path conf_path, &:read).read
   $blocks.publish
-  sleep
+  click_events_loop
 rescue SignalException
 rescue => e
   warn e.full_message
