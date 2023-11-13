0eb53d3bcab22c4349839da5681b47fcba89dd01
Futa Miyachi <miyachf@gmail.com>
2023-11-14 00:19:18 +0900

    multiple menus

--- i3blocks.rb
+++ i3blocks.rb
@@ -2,60 +2,106 @@
 
 require 'json'
 
-class Blocks
-  def initialize
-    @blocks_stack = []
+class BlockGroups
+  def initialize()
+    @current_group = nil
+    @groups = []
+    @mutex = Mutex.new
+  end
+
+  def push_group(key, group)
+    @groups.push([key, group])
+    @current_group = group if @current_group.nil?
+  end
+
+  def notified_update(by_group)
+    @mutex.synchronize {
+      draw() if by_group == @current_group
+    }
+  end
+
+  def switch_group(key)
+    @mutex.synchronize {
+      group = @groups.assoc(key.to_sym)&.at(1)
+      if group.nil?
+        warn "No such group of key: #{key}"
+        return
+      end
+      @current_group = group
+      draw()
+    }
+  end
+
+  def click(click_obj)
+    @current_group.click(click_obj)
+  end
+
+  def draw()
+    print(@current_group.to_j)
+    puts(',')
+    STDOUT.flush
+  end
+end
+
+class BlockGroup
+  def initialize(parent_groups)
+    @parent_groups = parent_groups
     @blocks = []
   end
 
-  def publish()
-    print '[', @blocks.join(', '), "],\n"
-    STDOUT.flush
+  def push(block)
+    @blocks.push(block)
   end
 
-  def push_blocks(blocks)
-    @blocks_stack.push(@blocks.clone)
-    @blocks = blocks
-    publish
+  def notified_update()
+    @parent_groups.notified_update(self)
   end
 
-  def pop_blocks()
-    @blocks = @blocks_stack.pop()
-    publish
+  def switch_group(key)
+    @parent_groups.switch_group(key)
   end
 
-  def click_event(click_obj)
-    @blocks
-      .find {|b| b.instance.to_s == click_obj['instance']}
-      &.on_click(click_obj)
+  def click(click_obj)
+    instance = click_obj['instance'].to_sym
+    @blocks.find {|b| b.instance == instance}&.click(click_obj)
+  end
+
+  def to_j
+    '[' + @blocks.map(&:to_j).join(',') + ']'
   end
 end
-$blocks = Blocks.new
 
 class Block
-  def initialize(instance, conf)
-    @command, @transformer, interval, props, onclick = conf.instance_eval {[@command, @transformer, @interval, @props, @onclick]}
-    props ||= {}
+  attr_reader :instance
+  def initialize(instance, parent_group, conf)
     @instance = instance
-    @block_obj = {instance: instance, full_text: '', **props}
+    @parent_group = parent_group
+    @command, @transformer, interval, props, onclick = conf.instance_eval {
+      [@command, @transformer, @interval, @props || {}, @onclick]
+    }
+
+    @block_obj = {instance: instance, **props}
+    @block_obj[:full_text] ||= ''
     @transformer ||= proc(&:chomp)
+
     unless @command.nil?
       case interval
-      in 'once' | :once | nil then exec_once
-      in 'persist' | :persist then set_persist
+      in 'once' | :once | nil then exec_once()
+      in 'persist' | :persist then set_persist()
       in Integer => n         then set_interval_by_secs(n)
       in Object => other      then raise "Unknown interval `#{other}` of block `#{name}`"
       end
     end
-    set_onclick onclick
-    regen_json
+    set_onclick(onclick)
+    update_json_buf()
   end
 
-  attr_reader :instance
-  def to_s = @buf
+  def to_j
+    @buf
+  end
 
-  def regen_json
-    @buf = JSON.generate @block_obj
+  def update_json_buf()
+    @buf = JSON.generate(@block_obj)
   end
 
   def update(content)
@@ -63,73 +109,66 @@ class Block
     in String => s then @block_obj[:full_text] = s
     in Hash => h then @block_obj.merge! h
     end
-    regen_json
-    $blocks.publish()
+    update_json_buf()
+    @parent_group.notified_update
   end
 
-  def update_by_command
-    warn "#{@instance} #{@transformer}"
-    update @transformer[`#{@command}`]
+  def update_by_command()
+    # warn "#{@instance} #{@transformer}"
+    update(@transformer[`#{@command}`])
   end
 
-  def exec_once
-    update_by_command
+  def exec_once()
+    update_by_command()
   rescue
-    update 'ERROR'
+    update('ERROR')
   end
 
   def set_interval_by_secs(secs)
     Thread.new do
       loop {
-        update_by_command
+        update_by_command()
         sleep secs
       }
     rescue => e
       warn e.full_message
-      update 'ERROR'
+      update('ERROR')
     end
   end
 
-  def set_persist
+  def set_persist()
     Thread.new do
       r, w = IO.pipe
       pid = spawn @command, out:w
-      r.each chomp:true do |out|
-        update @transformer[out]
-      end
+      r.each(chomp:true) {|out| update(@transformer[out])}
     rescue => e
       warn e.full_message
-      update 'ERROR'
+      update('ERROR')
     ensure
       Process.kill :TERM, pid
     end
   end
 
-  def push_submenu(blocks)
-    warn blocks
-    $blocks.push_blocks(blocks)
-  end
-
-  def pop_submenu
-    $block.pop_blocks()
-  end
-
   def set_onclick(onclick)
-    fn = case onclick
-         in '__back__'    then proc {|obj| pop_submenu}
-         in String => cmd then proc {|obj| spawn cmd}
-         in Proc => p     then proc {|obj| update p[obj]}
-         in Array => a    then proc {|obj| push_submenu a}
-         in nil           then proc {}
-         end
-    define_singleton_method 'on_click', &fn
+    procs = onclick&.map {|pair|
+      case pair
+      in [:do_cmd, cmd]       then proc {|obj| spawn cmd}
+#      in ['do_proc', p]       then proc {|obj| update p[obj]}
+      in [:switch_group, key] then proc {|obj| @parent_group.switch_group(key)}
+      in _                    then raise "Unexpected onclick value #{pair}"
+      end
+    } || []
+    fn = proc {|obj| procs.each{|pc| pc.(obj)}}
+    define_singleton_method('click', &fn)
   end
 end
 
 ## Config DSL
+
 class BlockConf
   def initialize(name)
     @name = name
+    @props = {}
   end
 
   def command(cmd, &trans)
@@ -141,22 +180,15 @@ class BlockConf
     @interval = intv
   end
 
-  def onclick(obj = nil, &block)
-    @onclick = if obj.nil? then block else obj end
+  def onclick(obj)
+    @onclick = case obj
+               in String => s then [[:do_cmd, s]]
+               in Array => a  then a
+               in Hash => h   then h
+               end
   end
 
-  def submenu(&block)
-    if block.nil? then raise "#{@name}: submenu block is missing" end
-    loader = ConfLoader.new
-    loader.instance_eval(&block)
-    back_block_conf = BlockConf.new('back')
-    back_block_conf.full_text('BACK') # tmp
-    back_block_conf.onclick('__back__')
-    back_block = Block.new('back', back_block_conf)
-    @onclick = loader.loaded_blocks + [back_block]
-  end
-
-  valid_props = %w(full_text short_text color background border border_top border_right
+  valid_props = %i(full_text short_text color background border border_top border_right
                    border_left border_bottom min_width align urgent separator
                    separator_block_width markup)
 
@@ -164,24 +196,46 @@ class BlockConf
     define_method prop do |*args|
       value, = args
       unless args.size == 1 then warn "block `#{@name}`: `#{name}` needs just one arg" end
-      @props ||= {}
       @props[prop] = value
     end
   }
 end
 
 class ConfLoader
-  attr_reader :loaded_blocks
-  def initialize
-    @loaded_blocks = []
+  class BlockLoader
+    attr_reader :loaded_blocks
+    def initialize(parent_group)
+      @parent_group = parent_group
+      @loaded_blocks = []
+    end
+
+    def method_missing(instance, *args, &body)
+      conf = BlockConf.new(instance)
+      conf.instance_eval(&body)
+      block = Block.new(instance, @parent_group, conf)
+      @loaded_blocks.push(block)
+    end
   end
 
-  def method_missing(name, *args, &body)
-    Kernel.warn "#{name}: args are ignored" unless args.empty?
-    conf = BlockConf.new(name)
-    conf.instance_eval &body
-    b = Block.new(name, conf)
-    @loaded_blocks.push b
+  class BlockGroupLoader
+    def initialize(groups)
+      @groups = groups
+    end
+
+    def method_missing(key, &body)
+      group = BlockGroup.new(@groups)
+      loader = BlockLoader.new(group)
+      loader.instance_eval(&body)
+      loader.loaded_blocks.each{|block| group.push(block)}
+      @groups.push_group(key, group)
+    end
+  end
+
+  def eval(s)
+    block_groups = BlockGroups.new()
+    loader = BlockGroupLoader.new(block_groups)
+    loader.instance_eval(s)
+    block_groups
   end
 end
 
@@ -203,11 +257,11 @@ def click_events
   }
 end
 
-def click_events_loop
+def click_events_loop(block_groups)
   if (i = STDIN.readchar) != '[' then raise "Invalid input: #{i}" end
   click_events.each do |obj|
     next if obj['instance'].nil?
-    $blocks.click_event(obj)
+    block_groups.click(obj)
   end
 end
 
@@ -217,10 +271,9 @@ begin
   conf_path ||= '~/.config/i3blocksrb/config.rb'
   puts JSON.generate ({version: 1, click_events: true})
   puts '['
-  loader = ConfLoader.new
-  loader.instance_eval open(File.expand_path conf_path, &:read).read
-  $blocks.push_blocks(loader.loaded_blocks)
-  click_events_loop
+  block_groups = ConfLoader.new.eval(open(File.expand_path conf_path, &:read).read)
+  block_groups.draw()
+  click_events_loop(block_groups)
 rescue SignalException
 rescue => e
   warn e.full_message
